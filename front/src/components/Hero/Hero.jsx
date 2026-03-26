ab9afbe965f560fd72ba0e29b3d810b530d6c557
MkhitarSiradeghyan <mkhitar.siradeghyan@yandex.ru>
2026-03-26 18:35:46 +0400

    Hero

--- front/src/components/Hero/Hero.jsx
+++ front/src/components/Hero/Hero.jsx
@@ -1,32 +1,19 @@
 import React from 'react'
 import s from "./Hero.module.sass"
-import cocktail from "@/assets/img/cocktail.png"
+import hero from "@/assets/img/hero.jpg"
 import Container from './../Container/Container';
 
 const Hero = () => {
   return (
-    <>
-    <section className={s.hero}>
+    <section className={s.hero} style={{backgroundImage: `url("${hero}")`}}>
         <Container>
             <div className={s.wrap}>
-                <h1 className={s.title}>Alcohol</h1>
-                <img src={cocktail} alt="Cocktail" />
-            </div>
-       
-            <div className={s.wrap}>
-                <h1 className={s.title}>Coffee</h1>
-            </div>
-       
-            <div className={s.wrap}>
-                <h1 className={s.title}>Tea</h1>
-            </div>
-       
-            <div className={s.wrap}>
-                <h1 className={s.title}>Fresh Drinks</h1>
+                <h1 className={s.title}>{import.meta.env.VITE_COMPANY_NAME}</h1>
+                <button className={s.btn}>Explore Drinks</button>
             </div>
         </Container>
     </section>
-    </>
+  
   )
 }
 
