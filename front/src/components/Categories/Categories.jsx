5f71c3e10fba4c99b93708acd11108efae5d8b28
MkhitarSiradeghyan <mkhitar.siradeghyan@yandex.ru>
2026-03-28 15:28:22 +0400

    FE - Categories fix

--- front/src/components/Categories/Categories.jsx
+++ front/src/components/Categories/Categories.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import s from "./Categories.module.sass"
 import Container from '../Container/Container'
 import coffee from "../../assets/img/coffee.png"
+import tea from "../../assets/img/tea.png"
+import alcohol from "../../assets/img/alcohol.png"
+import non_alcohol from "../../assets/img/non_alcohol.png"
 
 const Categories = () => {
   return (
@@ -15,15 +18,18 @@ const Categories = () => {
                 </div>
                 <div className={s.item}>
                     <h3 className={s.title}>Tea</h3>
-                    <div className={s.img}><img src="https://t4.ftcdn.net/jpg/02/27/88/19/360_F_227881943_SABhdakS4Tz1wrlguNriKjihNf5OVrun.jpg" alt="" /></div>
+                        <img src="https://t4.ftcdn.net/jpg/02/27/88/19/360_F_227881943_SABhdakS4Tz1wrlguNriKjihNf5OVrun.jpg" alt="" />
+<img src={tea} alt="" />
                 </div>
                 <div className={s.item}>
                     <h3 className={s.title}>Alcohol</h3>
-                    <div className={s.img}><img src="https://www.artofdrink.com/wp-content/uploads/2010/08/blue-lagoon-cocktail.jpg" alt="" /></div>
+                        <img src="https://www.artofdrink.com/wp-content/uploads/2010/08/blue-lagoon-cocktail.jpg" alt="" />
+<img src={alcohol} alt="" />
                 </div>
                 <div className={s.item}>
                     <h3 className={s.title}>Non Alcohol</h3>
-                    <div className={s.img}><img src="https://png.pngtree.com/thumb_back/fh260/background/20240418/pngtree-strawberry-milkshake-in-the-glass-jar-image_15664412.jpg" alt="" /></div>
+                        <img src="https://png.pngtree.com/thumb_back/fh260/background/20240418/pngtree-strawberry-milkshake-in-the-glass-jar-image_15664412.jpg" alt="" />
+<img src={non_alcohol} alt="" />
                 </div>
             </div>
         </Container>
