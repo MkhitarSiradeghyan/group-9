d3085782e7f891893f588aafb8325adc1a3f66cf
MkhitarSiradeghyan <mkhitar.siradeghyan@yandex.ru>
2026-04-21 18:45:02 +0400

    BE - Users Views

--- back/users/views.py
+++ back/users/views.py
@@ -1,5 +1,15 @@
-from django.http import HttpResponse
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
 
+@api_view(["post"])
+@permission_classes([AllowAny])
 
-def login(request):
-    return HttpResponse("Logged in successsfully")
+def register_user(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    email = request.data.get("email", "")
+    if not username or not password:
+        return Response({"error": "Username and password required"}, status=status.HTTP_400_BAD_REQUEST)
+    
